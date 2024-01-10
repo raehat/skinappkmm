5a84d599cd8c59310c9d2811d11cf3bb2beec01c
raehat <raehatsinghnanda@gmail.com>
2024-01-11 05:00:45 +0530

    add image picker

---
+++ composeApp/src/commonMain/kotlin/PhotoSelector/ImagePickerFactory.kt
@@ -0,0 +1,4 @@
+package PhotoSelector
+
+class ImagePickerFactory {
+}
---
+++ composeApp/src/iosMain/kotlin/ImagePickerFactory.native.kt
@@ -0,0 +1,8 @@
+package PhotoSelector
+
+actual class ImagePickerFactory {
+    @Composable
+    actual fun createPicker(): PhotoSelector.ImagePicker {
+        TODO("Not yet implemented")
+    }
+}
